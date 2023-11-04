53fc00aae35574957662d6d348a78baeddb61af5
CarrotManMatt <108596565+CarrotManMatt@users.noreply.github.com>
2023-11-04 13:07:41 +0000

    Fix settings.py formatting

--- core/settings.py
+++ core/settings.py
@@ -187,7 +187,7 @@ TEMPLATES = [
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3"
     }
 }
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
